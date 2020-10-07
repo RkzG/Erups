7db500b18dc71b223476f77a12625ed2c992b36a
Franky Wahyu <41984454+RkzG@users.noreply.github.com>
2020-10-07 22:14:17 +0700

    Add files via upload

---
+++ erups_question/static/src/js/login.js
@@ -0,0 +1,28 @@
+const inputs = document.querySelectorAll(".form-control");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+function yesnoCheck(that) {
+	if (that.value == "perwakilan") {
+		document.getElementById("ifYes").style.display = "block";
+	} else {
+		document.getElementById("ifYes").style.display = "none";
+	}
+}
